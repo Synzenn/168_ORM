bd64136a2955f109d8a52d50459ceffba72590a5
Synzenn <velriel42@gmail.com>
2025-10-28 14:59:51 +0700

    membuat app.listen

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
